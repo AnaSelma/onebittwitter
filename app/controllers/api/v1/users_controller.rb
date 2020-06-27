bfd641d197fbcf9f03a6318747b999dc967dd477
Ana Selma <anaselma1962@gmail.com>
2020-06-27 18:03:49 -0300

    create controllers and configure routes

---
+++ app/controllers/api/v1/users_controller.rb
@@ -0,0 +1,22 @@
+class Api::V1::UsersController < ApplicationController
+  def create
+  end
+
+  def destry
+  end
+
+  def update
+  end
+
+  def current
+  end
+
+  def show
+  end
+
+  def following
+  end
+
+  def followers
+  end
+end
